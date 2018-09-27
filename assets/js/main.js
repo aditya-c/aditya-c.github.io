e898a7339221648ca8145e4e427cf71aa06fbd55
Aditya C <saiadityac@gmail.com>
2018-09-26 20:41:05 -0700

    + random error fix

--- assets/js/main.js
+++ assets/js/main.js
@@ -54,9 +54,4 @@ jQuery(document).ready(function($) {
         }
     );
 
-
-    /* Github Activity Feed - https://github.com/caseyscarborough/github-activity */
-    GitHubActivity.feed({ username: "caseyscarborough", selector: "#ghfeed" });
-
-
 });
